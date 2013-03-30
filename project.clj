a2c00b9f3c9fec27e8319703f8b183df4a7f54cc
Assen Kolov <assen.kolov@gmail.com>
2013-03-30 15:17:07 +0100

    heroku

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
     :dependencies [[org.clojure/clojure "1.5.0"]
                    [org.clojure/data.zip "0.1.1"]
                    [ring/ring-core "1.2.0-beta1"]
+                   [ring/ring-jetty-adapter "1.1.0"]
                    [compojure "1.2.0-SNAPSHOT"]
                    [enlive "1.1.1"]
                    [com.cemerick/friend "0.1.5"]]
@@ -12,5 +13,6 @@
     :plugins [[lein-ring "0.8.3"]]
     :source-paths ["src/clj"] 
     :resource-paths ["resources/public"]
+    :main civi.core
     :ring {:handler civi.core/secured-app}
  )
